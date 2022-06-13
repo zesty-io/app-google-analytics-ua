7df1496792bfbf22310922d6537807e615940b28
Allen Pigar <50983144+allenpigar@users.noreply.github.com>
2022-06-13 22:02:40 +0800

    porting existing google analytics app, shell application using react app loader

---
+++ src/components/Analytics/components/GoogleAuthOverlay/GaAuthenticate.js
@@ -0,0 +1,32 @@
+import { Fragment } from "react";
+
+// import {usePermission} from "../../../../shell/hooks/use-permissions"
+
+import Button from "@mui/material/Button";
+
+import KeyIcon from "@mui/icons-material/Key";
+
+import styles from "./GoogleAuthOverlay.less";
+
+export default function GaAuthenticate(props) {
+  // const canAuthenticate = usePermission("CODE");
+  const canAuthenticate = true
+
+
+  return (
+    <Fragment>
+      {canAuthenticate && (
+        <div className={styles.buttonHolder}>
+          <Button
+            variant="contained"
+            color="success"
+            onClick={props.onClick}
+            startIcon={<KeyIcon />}
+          >
+            Click here to Authenticate With Google
+          </Button>
+        </div>
+      )}
+    </Fragment>
+  );
+}
---
+++ src/utility/request.js
@@ -0,0 +1,109 @@
+import Cookies from "js-cookie";
+// import { store } from "shell/store";
+// import { endSession } from "shell/store/auth";
+
+export function request(url, opts = {}) {
+
+ 
+
+  if (!url) {
+    throw new Error("A URL is required to make a request");
+  }
+
+  opts.headers = opts.headers || {};
+  // opts.headers["Authorization"] = `Bearer ${Cookies.get(CONFIG.COOKIE_NAME)}`;
+  opts.headers["Authorization"] = `Bearer ${Cookies.get(process.env.REACT_APP_COOKIE_NAME)}`;
+
+  if (!opts.method && opts.body) {
+    opts.method = "POST";
+  }
+
+  if (opts.body) {
+    if (
+      opts.headers &&
+      (opts.headers["Content-Type"] || opts.headers["content-type"])
+    ) {
+      console.log(
+        `Custom content-type: ${
+          opts.headers["Content-Type"] || opts.headers["content-type"]
+        }`
+      );
+    } else {
+      if (opts.json) {
+        opts.headers["Content-Type"] = "application/json";
+        opts.body = JSON.stringify(opts.body);
+      } else {
+        let formData = new FormData();
+        // TODO: Add header support
+
+        for (var key in opts.body) {
+          if (opts.body.hasOwnProperty(key)) {
+            formData.append(key, opts.body[key]);
+          }
+        }
+
+        opts.body = formData;
+      }
+    }
+  }
+
+  opts.credentials = opts.credentials || "omit";
+  opts.method = opts.method || "GET";
+
+  console.log(opts)
+
+  return fetch(url, opts)
+    .then((res) => {
+      // console.log("Request:", res);
+
+      //HTTP Code No Content 204
+      if (res.status === 204) {
+        return res;
+      }
+
+      // // Bad Request
+      // if (res.status === 400) {}
+
+      // if (res.status === 401) {
+      //   store.dispatch(endSession());
+      //   throw new Error(`401:Unauthenticated: ${res.url}`);
+      // }
+
+      // Not Found
+      // if (res.status === 404) {}
+
+      // if (res.status === 410) {
+      //   // notify({message: `Your two factor authentication has expired. 410`});
+      // }
+
+      // if (res.status === 422) {}
+
+      if (res.status === 500) {
+        // Throw on 500 to trigger callers catch statement
+        throw new Error(`500:RequestError: ${res.url}`);
+      }
+
+      return res.json().then(function (json) {
+        return { ...json, status: res.status };
+      });
+    })
+    .then((json) => {
+      if (opts.callback) {
+        opts.callback(json);
+      }
+      return json;
+    })
+    .catch((err) => {
+      if (err && err.res && err.res.status >= 500) {
+        // Server Failed
+        console.error(
+          `${err.opts.method.toUpperCase()} ${err.url} | Code: ${
+            err.res.status
+          }`
+        );
+        throw err;
+      } else {
+        throw err;
+      }
+    });
+}
