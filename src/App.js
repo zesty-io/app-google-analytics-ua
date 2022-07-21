e3cf99d053770183719981fe4309327c1d0a9f37
Allen Pigar <50983144+allenpigar@users.noreply.github.com>
2022-07-21 20:58:17 +0800

    fix dataset ticks format

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import AppWrapper from "./components/ui/AppWrapper/AppWrapper";
 function App() {
 
 	return (
-		<AppLoader>
+		<AppLoader
+			token={process.env.REACT_APP_TOKEN}
+			instance={{
+					ZUID: `${process.env.REACT_APP_ZUID}`, 
+					ID : process.env.REACT_APP_INSTANCE_ID
+				}}>
 			<AppWrapper />
 		</AppLoader>
 	);
