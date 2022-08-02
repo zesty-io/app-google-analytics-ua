5ff51ba9c5c11fcd98fb04a2eb6955de4c630618
Allen Pigar <50983144+allenpigar@users.noreply.github.com>
2022-08-02 20:12:55 +0800

    fix unnecessary call for api when slice and url changes

---
+++ src/components/Journey/JourneyGraph.js
@@ -0,0 +1,167 @@
+import { useEffect, useState } from "react"
+import { Chart } from "react-google-charts";
+import { Box } from "@mui/material"
+
+export default function JourneyGraph({ data, filter, slice }){
+
+    const [graphData, setGraphData] = useState(null)
+    const [summary, setSummary] = useState(null)
+
+    useEffect(() => {
+        if(data){
+            const formatData = formatJourney(data.googleData)
+            setGraphData(formatData)
+        }
+    }, [data, filter, slice])
+
+    const formatJourney = (data) => {
+
+        let raw = data.reports[0].data.rows
+        let totals = data.reports[0].data.totals[0].values[1]
+        let levels = {}
+        let i = 1
+        
+        if(!raw) return null
+
+        do{
+            if(i === 1){
+                var filterData = raw.filter(item => item.dimensions[0] === "(entrance)")
+                raw = removeArray(raw, filterData)
+                levels[`Level${i}`] = filter !== null ? filterData.filter(item => item.dimensions[1] === filter.label).slice(0, slice) : filterData.slice(0, slice)
+            }else{
+                let bufferData = []
+                levels[`Level${i - 1}`].forEach((data, i) => {
+                    let levelData = raw.filter(item => data.dimensions[1] === item.dimensions[0])
+                    if(levelData.length !== 0) bufferData.push(...levelData)
+                })
+                raw = removeArray(raw, bufferData)
+                levels[`Level${i}`] = bufferData.sort(compare).slice(0, slice)
+            }
+            i = i + 1
+        }
+        while(i < 20)
+
+        let sankeyData = [["From", "To", "Visit"]]
+        for (var key in levels) {
+            if (levels.hasOwnProperty(key)) {
+                levels[key].forEach(item => {
+                    if(item.dimensions[0] !== item.dimensions[1] && !ifSankeyExist(sankeyData, item)) sankeyData.push([item.dimensions[0], item.dimensions[1], Number(item.metrics[0].values[1])])
+                })
+            }
+        }
+
+        const maxEntrance = sankeyData.filter(value => value[0] === '(entrance)').reduce((prev, current) => (prev[2] > current[2] ? prev : current), [])
+        const maxPage = sankeyData.filter(value => value[0] === maxEntrance[1]).reduce((prev, current) => (prev[2] > current[2] ? prev : current), [])
+       
+        setSummary({
+            entrance : maxEntrance,
+            page : maxPage,
+            aveVisit : ave(totals, maxPage.length !== 0 ? maxPage[2] : maxEntrance[2])
+        })
+        return sankeyData
+    }
+
+    const ave = (a, b) => {
+        return ((b / a) * 100).toFixed(2)
+    }
+
+    const ifSankeyExist = (base, data) => {
+        
+        let value = false
+
+        base.forEach(items => {
+            if(items[0] === data.dimensions[0] && items[1] === data.dimensions[1]){
+                value = true
+            }
+            if(items[0] === data.dimensions[1]){
+                value = true
+            } 
+            
+        })
+
+        return value
+
+    }
+
+    const compare = (a, b) => {
+        if ( a.metrics[0].values[1] > b.metrics[0].values[1]){
+            return -1;
+        }
+        if ( a.metrics[0].values[1] < b.metrics[0].values[1]){
+            return 1;
+        }
+        return 0;
+    }
+
+    const removeArray = (base, remove) => {
+        let newArray = []
+
+        base.forEach(item => {
+            if(!remove.includes(item)) newArray.push(item)
+        })
+
+        return newArray
+    }
+
+    var colors = [
+        '#7a56ff', 
+        '#36a2eb', 
+        '#ffce56', 
+        '#41ead4', 
+        '#ff0022', 
+        '#56ff7a', 
+        '#ff56db', 
+        '#ff7a56', 
+        '#56dbff', 
+        '#7a56ff', 
+        '#36a2eb', 
+        '#ffce56', 
+        '#41ead4', 
+        '#ff0022', 
+        '#56ff7a', 
+        '#ff56db', 
+        '#ff7a56', 
+        '#56dbff'];
+
+    var options = {
+        sankey: { 
+            node: { 
+                nodePadding: 20,
+                colors: colors
+            },
+            link: {
+                colorMode: 'gradient',
+                colors: colors
+            }
+        },
+    };
+
+
+    return (
+        <>
+            <Box sx={{ width : "100%", height: 1000 }}>
+                {graphData !== null && (
+                    <>
+                    
+                        <Box
+                            sx={{
+                                textAlign : "right",
+                                marginBottom : "20px"
+                            }}>
+                            { summary.page.length !== 0 && `The top page "${summary.page[1]}" from the "${summary.entrance[1]}" entrance received ${summary.page[2]} visits (${summary.aveVisit}%) of traffic flow`}
+                            { summary.page.length === 0 && `The page "${summary.entrance[1]}" received ${summary.entrance[2]} visits (${summary.aveVisit}%) of traffic flow`}
+                        </Box>
+                        <Chart 
+                            chartType="Sankey"  
+                            width="100%"
+                            height={800}
+                            data={graphData === null ? [["From", "To", "Visit"], ["From", "To", 0],] : graphData}
+                            options={options} />
+                        
+                    </>
+                )}
+            </Box>
+        </>
+    )
+
+}
--- src/components/Journey/index.js
+++ src/components/Journey/index.js
@@ -8,28 +8,29 @@ import { Chart } from "react-google-charts";
 import Slicer from "./Slicer";
 import EntranceSelector from "./EntranceSelector";
 import { useNotify } from "../../context/SnackBarContext";
+import JourneyGraph from "./JourneyGraph";
 
 
 export default function Journey({instance, token}){
     const notify = useNotify();
     const { googleDetails } = useGoogle()
     const dateRange = useDateRange() 
-    const [data, setData] = useState(null)
     const { getPageJourney } = useAnalyticsApi(instance.ZUID)
     const [isLoading, setIsLoading] = useState(false)
     const [ slice, setSlice ] = useState(10)
     const [ url, setUrl ] = useState(null)
     const [ entranceUrl, setEntranceUrl ] = useState([])
-    const [summary, setSummary] = useState(null)
+    const [rawData, setRawData] = useState(null)
 
     useEffect(async () => {
         if(googleDetails){
             try{
                 setIsLoading(true)
                 const responseJourney = await getPageJourney(googleDetails.defaultProfileId, dateRange)
-                const journey = formatJourney(responseJourney.googleData)
                 const entrances = getEntranceUrl(responseJourney.googleData)
-                setData(journey)
+                console.log(responseJourney)
+
+                setRawData(responseJourney)
                 setEntranceUrl(entrances)
                 setIsLoading(false)
             }catch (error) {
@@ -39,7 +40,7 @@ export default function Journey({instance, token}){
               }
             
         }
-    }, [dateRange, googleDetails, slice, url])  
+    }, [googleDetails, dateRange])  
 
     const debounce = (func) => {
         let timer;
@@ -74,105 +75,6 @@ export default function Journey({instance, token}){
         })
 
         return url
-
-
-    }
-
-    const formatJourney = (data) => {
-
-        
-        let raw = data.reports[0].data.rows
-        let totals = data.reports[0].data.totals[0].values[1]
-        let levels = {}
-        let i = 1
-        if(!raw){
-            setSummary(null)
-            return [["From", "To", "Visit"], ["No Data 1", "No Data 2", 0]]
-        } 
-        do{
-            if(i === 1){
-                var filterData = raw.filter(item => item.dimensions[0] === "(entrance)")
-                raw = removeArray(raw, filterData)
-                levels[`Level${i}`] = url !== null ? filterData.filter(item => item.dimensions[1] === url.label).slice(0, slice) : filterData.slice(0, slice)
-            }else{
-                let bufferData = []
-                levels[`Level${i - 1}`].forEach((data, i) => {
-                    let levelData = raw.filter(item => data.dimensions[1] === item.dimensions[0])
-                    if(levelData.length !== 0) bufferData.push(...levelData)
-                })
-                raw = removeArray(raw, bufferData)
-                levels[`Level${i}`] = bufferData.sort(compare).slice(0, slice)
-            }
-            i = i + 1
-        }
-        while(i < 20)
-
-        let sankeyData = [["From", "To", "Visit"]]
-        for (var key in levels) {
-            if (levels.hasOwnProperty(key)) {
-                levels[key].forEach(item => {
-                    if(item.dimensions[0] !== item.dimensions[1] && !ifSankeyExist(sankeyData, item)) sankeyData.push([item.dimensions[0], item.dimensions[1], Number(item.metrics[0].values[1])])
-                    
-                })
-            }
-        }
-
-        const maxEntrance = sankeyData.filter(value => value[0] === '(entrance)').reduce((prev, current) => (prev[2] > current[2] ? prev : current), [])
-        const maxPage = sankeyData.filter(value => value[0] === maxEntrance[1]).reduce((prev, current) => (prev[2] > current[2] ? prev : current), [])
-        
-        if(maxEntrance.length !== 0 && maxPage.length !== 0){
-            setSummary({
-                entrance : maxEntrance,
-                page : maxPage,
-                aveVisit : ave(totals, maxPage.length !== 0 ? maxPage[2] : maxEntrance[2])
-            })
-    
-        }
-
-        return sankeyData
-    }
-
-    const ave = (a, b) => {
-        console.log(a, b)
-        return ((b / a) * 100).toFixed(2)
-    }
-
-    const ifSankeyExist = (base, data) => {
-        
-        let value = false
-
-        base.forEach(items => {
-            if(items[0] === data.dimensions[0] && items[1] === data.dimensions[1]){
-                value = true
-            }
-            if(items[0] === data.dimensions[1]){
-                value = true
-            } 
-            
-        })
-
-        return value
-
-    }
-
-    const compare = (a, b) => {
-        if ( a.metrics[0].values[1] > b.metrics[0].values[1]){
-            return -1;
-        }
-        if ( a.metrics[0].values[1] < b.metrics[0].values[1]){
-            return 1;
-        }
-        return 0;
-    }
-
-    const removeArray = (base, remove) => {
-        let newArray = []
-
-        base.forEach(item => {
-            if(!remove.includes(item)) newArray.push(item)
-        })
-
-        return newArray
     }
 
     const RightComponent = () => (
@@ -182,57 +84,11 @@ export default function Journey({instance, token}){
         </Box>
     )
 
-    var colors = [
-            '#7a56ff', 
-            '#36a2eb', 
-            '#ffce56', 
-            '#41ead4', 
-            '#ff0022', 
-            '#56ff7a', 
-            '#ff56db', 
-            '#ff7a56', 
-            '#56dbff', 
-            '#7a56ff', 
-            '#36a2eb', 
-            '#ffce56', 
-            '#41ead4', 
-            '#ff0022', 
-            '#56ff7a', 
-            '#ff56db', 
-            '#ff7a56', 
-            '#56dbff'];
-
-    var options = {
-        sankey: { 
-            node: { 
-                nodePadding: 20,
-                colors: colors
-            },
-            link: {
-                colorMode: 'gradient',
-                colors: colors
-              }
-        },
-    };
 
     return (
         <>
             <GraphContainer title="User Journey - Beta" subTitle={dateRange.selectedItem} loading={isLoading} rightMenu={<RightComponent />}>
-                {summary !== null && (
-                    <Box
-                        sx={{
-                            textAlign : "right",
-                            marginBottom : "20px"
-                        }}>
-                        {`The top page "${summary.page[1]}" from the "${summary.entrance[1]}" entrance received ${summary.page[2]} visits (${summary.aveVisit}%) of traffic flow`}
-                    </Box>
-                )}
-                <Chart 
-                 chartType="Sankey"
-                 width="100%"
-                 height={800}
-                 data={data === null ? [["From", "To", "Visit"], ["From", "To", 0],] : data}
-                 options={options} />
+                <JourneyGraph data={rawData} filter={url} slice={slice} />
             </GraphContainer>
         </>
     )
